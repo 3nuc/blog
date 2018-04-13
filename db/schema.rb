bf25ee7bcbe2c9ed6b32e3fe400d418a92b0c4f9
MaxiMHc <maximgt540@gmail.com>
2018-04-13 13:30:19 +0200

    Add database structure, basic users controller, user api hyperlinks to root

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407162551) do
+ActiveRecord::Schema.define(version: 20180413104646) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogbases", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogbases_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "poster_name"
+    t.text "content"
+    t.integer "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_comments_on_entry_id"
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.string "password"
+    t.integer "blogbase_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blogbase_id"], name: "index_entries_on_blogbase_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
